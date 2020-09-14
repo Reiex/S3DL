f3cda091f6f72c77d31b42ea7555aa19083767ff
Reiex <reiex.marius38@gmail.com>
2020-09-14 13:42:02 +0200

    Header textures

--- include/S3DL/Texture.hpp
+++ include/S3DL/Texture.hpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <cstring>
 #include <cstdint>
+#include <array>
+#include <unordered_map>
 
 #include <vulkan/vulkan.h>
 
@@ -28,17 +30,93 @@ namespace s3dl
 			mutable VkSampler _vulkanSampler;
 	};
 
+	class TextureViewParameters
+	{
+		public:
+
+			TextureViewParameters(VkImageAspectFlags aspects, std::array<uint32_t, 2> layerRange = {0, 1});
+
+			void setFormat(VkFormat format);
+			void setViewType(VkImageViewType viewType);
+			void setComponentMapping(VkComponentSwizzle r, VkComponentSwizzle g, VkComponentSwizzle b, VkComponentSwizzle a = VK_COMPONENT_SWIZZLE_IDENTITY);
+
+			VkImageViewCreateInfo getVulkanImageViewCreateInfo() const;
+
+		private:
+
+			VkImageViewCreateInfo _view;
+	};
+
 	class TextureArray
 	{
 		public:
 
 			TextureArray(const uvec2& size, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, uint32_t layerCount);
+			TextureArray(const TextureArray& textureArray) = delete;
 
-		private:
+			TextureArray& operator=(const TextureArray& textureArray) = delete;
+
+            void fillFromTextureData(const TextureData& textureData, uint32_t layer);
+            void fillFromBuffer(const Buffer& buffer, uint32_t firstLayer, uint32_t layerCount);
+            void fillFromTextureArray(const TextureArray& textureArray, uint32_t srcFirstLayer, uint32_t dstFirstLayer, uint32_t layerCount);
+			void fillFromTexture(const Texture& texture, uint32_t dstLayer);
+
+			void setSampler(const TextureSampler& sampler);
+
+            void updateLayoutState(VkImageLayout layout) const;
+            void setLayout(VkImageLayout layout) const;
+
+            TextureData getTextureData(uint32_t layer = 0) const;
+            const uvec2& getSize() const;
+
+            VkImage getVulkanImage() const;
+            VkImageView getVulkanImageView(TextureViewParameters viewParameters) const;
+            VkSampler getVulkanSampler() const;
+
+            ~TextureArray();
+
+		protected:
+
+            uvec2 _size;
+			uint32_t _layerCount;
+			VkFormat _format;
+			VkImageTiling _tiling;
+			VkImageUsageFlags _usage;
+
+            VkDeviceMemory _vulkanImageMemory;
+            VkImage _vulkanImage;
+			std::unordered_map<TextureViewParameters, VkImageView> _vulkanImageViews;
+			TextureSampler* _sampler;
+
+            mutable VkImageLayout _currentLayout;
 	};
 
-	class Texture: public TextureArray
+	class Texture: private TextureArray
 	{
+		public:
 
+			Texture(const uvec2& size, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage);
+			Texture(const Texture& texture) = delete;
+
+			Texture& operator=(const Texture& texture) = delete;
+
+            void fillFromTextureData(const TextureData& textureData);
+            void fillFromBuffer(const Buffer& buffer, uint32_t firstLayer);
+            void fillFromTextureArray(const TextureArray& textureArray, uint32_t srcLayer);
+			void fillFromTexture(const Texture& texture);
+
+			void setSampler(const TextureSampler& sampler);
+
+            void updateLayoutState(VkImageLayout layout) const;
+            void setLayout(VkImageLayout layout) const;
+
+            TextureData getTextureData() const;
+            const uvec2& getSize() const;
+
+            VkImage getVulkanImage() const;
+            VkImageView getVulkanImageView(TextureViewParameters viewParameters) const;
+            VkSampler getVulkanSampler() const;
+
+            ~Texture();
 	};
 }
